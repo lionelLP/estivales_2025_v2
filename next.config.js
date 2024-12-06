93272343ab16fe9bbad065d309f03b8532400f02
benjamin.brajon@etu.univ-lyon1.fr <benjamin.brajon@etu.univ-lyon1.fr>
2024-12-06 15:10:05 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/uploads/**',
-      },
+    domains: [
+      'images.unsplash.com',
+      'localhost',
+      'estivales-brou.vercel.app'
     ],
   },
 }
