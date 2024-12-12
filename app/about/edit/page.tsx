2da864bbde11860887853212c46245174104bdba
benjamin.brajon@etu.univ-lyon1.fr <benjamin.brajon@etu.univ-lyon1.fr>
2024-12-12 09:28:42 +0100

    text alignement about editor

--- app/about/edit/page.tsx
+++ app/about/edit/page.tsx
@@ -16,10 +16,12 @@ const modules = {
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
     ["bold", "italic", "underline", "strike"],
     [{ list: "ordered" }, { list: "bullet" }],
+    [{ align: [] }],
     [{ color: [] }, { background: [] }],
     ["link", "image"],
     ["clean"],
-  ],
+    ["code-block"],
+    ],
 };
 
 export default function EditAbout() {
