6ad465576abd880df9b27b0e4dde63cf31ac0e00
valentin <valentin.goux@etu.univ-lyon1.fr>
2025-03-20 16:26:59 +0100

    fix dialog tilte

--- app/admin/medias/medias.tsx
+++ app/admin/medias/medias.tsx
@@ -488,6 +488,7 @@ export default function MediasPage() {
       {/* Modal pour visionner les vidéos YouTube */}
       <Dialog open={isVideoModalOpen} onOpenChange={setIsVideoModalOpen}>
         <DialogContent className="sm:max-w-[800px] max-h-[90vh] p-0">
+          <DialogTitle className="sr-only">Lecteur vidéo YouTube</DialogTitle>
           <div className="p-4 relative">
             <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
               <X className="h-4 w-4" />
