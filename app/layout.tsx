ac9d7d7f35a54e2cd431ee2f9e44e2d0b07ebb38
Thomas <thomas.perrin2@etu.univ-lyon1.fr>
2025-12-11 09:40:27 +0100

    Fix problème de build

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fr" className={`${poppins.variable}`}>
+    <html lang="fr" className={`${poppins.variable}`} suppressHydrationWarning>
       <head>
         <link
           rel="stylesheet"
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    turbopack: {},
     webpack: (config, { dev, isServer }) => {
         if (dev && !isServer) {
             config.watchOptions = {
