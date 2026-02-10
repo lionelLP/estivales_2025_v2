b5e5efb43a6bb864128b87e6e4cb4bcc82660ef3
anaskad01 <anaskadar.01@icloud.com>
2026-02-10 11:05:57 +0100

    [ADD] Choriste list with API and display in infos page

---
+++ app/espace-choriste/infos/page.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Infos choristes | Estivales de Brou",
+  description: "Liste des choristes et coordonnées.",
+};
+
+export { default } from "./infos";
