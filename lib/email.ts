e459fe33982741a99ceb041345ad90e624461636
Lionel <Lionel Banand>
2026-03-30 13:33:15 +0200

    fix email syntax error

--- lib/email.ts
+++ lib/email.ts
@@ -30,8 +30,7 @@ const transportOptions: SMTPTransport.Options = {
         user: process.env.SMTP_USER || "",
         pass: process.env.SMTP_PASS || "",
       },
-} as any);
-};
+} as any;
 
 const transporter = nodemailer.createTransport(transportOptions);
 
