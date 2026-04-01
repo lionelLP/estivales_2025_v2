fc6f9909057a71dc00b9372108f88f04ea094019
Lionel <Lionel Banand>
2026-04-01 11:26:54 +0200

    fix images size

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     allowedDevOrigins: ['127.0.0.1'],
-    // === LES PILULES ANTI-CRASH MÉMOIRE ===
+    experimental: {
+        serverActions: {
+        bodySizeLimit: '10mb',
+        },
+    },
     typescript: {
         ignoreBuildErrors: true,
     },
